1e8de65cece4cf364ccc44892ea117c3eba6baa1
Sebastian Marin <sebastian.1546@gmail.com>
2023-06-13 14:34:41 -0500

    Revert "add Google Analytics"
    This reverts commit 3ea95518feed8d878a0ee3ed4d235b339538de23.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import "@/src/scss/global.scss";
 import { roboto, sourceCodePro, archivo } from "@/src/fonts";
 import { WEBSITE_NAME, WEBSITE_DESCRIPTION } from "@/constants";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio.js";
 
@@ -16,26 +16,6 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${roboto.variable} ${sourceCodePro.variable} ${archivo.variable} font-body text-root-black`}
     >
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){
-              dataLayer.push(arguments);
-              }
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-            </script>
-            `,
-          }}
-        />
-      </Head>
       <body suppressHydrationWarning={true}>
         <PrismicPreview repositoryName={repositoryName}>
           {children}
